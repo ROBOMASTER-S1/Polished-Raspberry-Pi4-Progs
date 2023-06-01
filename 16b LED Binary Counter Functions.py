9adcb2a68d77e9302bf71bc281029a320bc18021
Welcome To My Virtual Computer Science Research Laboratory <josephcrichardson08@gmail.com>
2023-06-01 11:00:43 -0400

    Update 16b LED Binary Counter Functions.py

--- 16b LED Binary Counter Functions.py
+++ 16b LED Binary Counter Functions.py
@@ -136,5 +136,5 @@ except KeyboardInterrupt:
     exec(stop_program_message) # GPIO notification message
 
     clear_all_data_bits() # call the function
-
-    GPIO.cleanup() # GPIO.cleanup() sets all GPIO pins to LOW/OFF
+    
+GPIO.cleanup() # GPIO.cleanup() sets all GPIO pins to LOW/OFF
